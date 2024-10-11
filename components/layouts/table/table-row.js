929e11740bb0bc2b9979933aa9c549d912aa811a
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-11 18:46:31 +0100

    initial commit for admin

--- components/layouts/table/table-row.js
+++ components/layouts/table/table-row.js
@@ -1,10 +1,15 @@
 /** @format */
-
+'use client'
 import React from "react";
 
 export default function TableRow({ data }) {
+
+    const handleClick = () => {
+        console.log("CLICK")
+    }
+
   return (
-    <tr>
+    <tr onClick={handleClick}>
       {data.map((item) => {
         return <td key={item}>{item}</td>;
       })}
