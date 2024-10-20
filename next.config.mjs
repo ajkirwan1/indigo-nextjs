331c06f4868e42952d89339719bf62216842202f
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-20 08:49:53 +0100

    fgh

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,12 @@
  */
 
 const nextConfig = {
-  // experimental: {
-  //   serverComponentsExternalPackages: ["@node-rs/argon2"],
-  // },
+  experimental: {
+    serverComponentsExternalPackages: ["@node-rs/argon2"],
+  },
+  outputFileTracingIncludes: {
+    "/": ["../../node_modules/argon2/prebuilds/linux-x64/*"],
+  },
 };
 
 export default nextConfig;
