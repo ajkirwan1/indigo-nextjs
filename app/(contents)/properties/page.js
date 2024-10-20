779d3a12dac8561db57ec659522f2f84b09c579d
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-20 08:34:35 +0100

    ewrwer

--- app/(contents)/properties/page.js
+++ app/(contents)/properties/page.js
@@ -3,14 +3,14 @@ import { redirect } from "next/navigation";
 import withAuthentication from "@/components/withAuthentication";
 import PropertyList from "@/components/pages/properties/property-list";
 import { validateRequest } from "/auth/lucia";
-import { getProperties } from "@/server/actions/db/properties";
+// import { getProperties } from "@/server/actions/db/properties";
 import classes from "./page.module.css"
 import { headers } from "next/headers";
 
-async function Properties() {
-  const properties = await getProperties();
-  return <PropertyList properties={properties} />;
-}
+// async function Properties() {
+//   const properties = await getProperties();
+//   return <PropertyList properties={properties} />;
+// }
 
 async function PropertiesPage() {
 
@@ -28,7 +28,7 @@ async function PropertiesPage() {
       <div className={classes.subHeader}>
         <h1>PROPERTIES FOR SALE</h1>
       </div>
-      <Properties />
+      {/* <Properties /> */}
     </>
   );
 }
