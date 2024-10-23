330e21b965d0e9ce5bd389123b1d9fb0d31cc86e
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-23 13:20:16 +0100

    gfdg

--- seeds/seed_user.js
+++ seeds/seed_user.js
@@ -18,7 +18,7 @@ export async function seed(knex) {
     {
       id: userId1,
       username: "user123",
-      firstname: "micsadasdsadsak",
+      firstname: "Michael",
       lastname: "jackson",
       email: "user@123.com",
       adminaccess: 0,
