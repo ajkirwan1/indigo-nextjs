ea041f59d159ca2a74f7622cc900bfb80031df51
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-23 12:58:33 +0100

    updated

--- components/ui/header/header.js
+++ components/ui/header/header.js
@@ -1,23 +1,25 @@
 /** @format */
 "use client";
 
+import IndigoLogo from "/public/Indigo_Logo_Transparent.png";
 import classes from "./header.module.css";
 import NavLink from "@/components/nav-link";
 import { useSession } from "@/contexts/session-context";
-import  MobileMenuIcon  from "./mobile-menu-icon";
-import  MobileNavbar  from "./mobile-nav";
+import MobileMenuIcon from "./mobile-menu-icon";
+import MobileNavbar from "./mobile-nav";
 import DesktopNav from "./desktop-nav";
 import LayoutContext from "@/contexts/layout-context";
 import { navigationData } from "@/data/navigation-data";
 import { useContext, useEffect } from "react";
 import { adminNavigationData } from "@/data/admin-navigation-data";
+import Image from "next/image";
 
 export default function Header({ className }) {
   const { handleLayoutChange, showMobileNavMenu, mobileMenuOpen } =
     useContext(LayoutContext);
 
-  const {user} = useSession();
-    
+  const { user } = useSession();
+
   useEffect(() => {
     document.body.style.overflow = "scroll";
     if (window.innerWidth > 767) {
@@ -46,11 +48,16 @@ export default function Header({ className }) {
       <header className={className}>
         <nav className={classes.nav}>
           <NavLink href="/">
-            <img className={classes.logoIndigo} src="./Indigo_Logo_Transparent.png"></img>
+            <Image
+              src={IndigoLogo}
+              alt="The logo for indigo"
+              className={classes.logoIndigo}
+            />
           </NavLink>
-
           {!showMobileNavMenu ? (
-            <DesktopNav data={user?.adminUser ? adminNavigationData : navigationData}></DesktopNav>
+            <DesktopNav
+              data={user?.adminUser ? adminNavigationData : navigationData}
+            ></DesktopNav>
           ) : (
             <MobileMenuIcon />
           )}
