42c48b2ed182710b2cf0b55be9e0491cceda5202
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-23 19:51:41 +0100

    first commit

--- components/forms/logout-form.js
+++ components/forms/logout-form.js
@@ -1,15 +1,22 @@
 /** @format */
 
 "use client";
+import indigoLogo from "/public/Indigo_Logo_Transparent.png";
 import { useFormState } from "react-dom";
 import FormSubmit from "./formsubmit";
 import classes from "./login-form.module.css";
+import Image from "next/image";
+
 export default function LogoutForm({ action }) {
   const [state, formAction] = useFormState(action, {});
 
   return (
     <>
-      <img className={classes.logoIndigo} src="./logoindigo.png"></img>
+      <Image
+        className={classes.logoIndigo}
+        src={indigoLogo}
+        alt="An image displayinging the logo for Indigo"
+      />
       <h1>Logout</h1>
       <form className={classes.loginForm} action={formAction}>
         <div className={classes.submitButtonContainer}>
