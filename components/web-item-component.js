6bface86e7d7f26abf88e7b57030c69b12092cd8
Adam Kirwan <ajkirwan1@gmail.com>
2024-10-24 16:00:30 +0100

    wqeweq

--- components/web-item-component.js
+++ components/web-item-component.js
@@ -13,26 +13,40 @@ export default function WebItemComponent(props) {
           : classes.sectionContainer
       }
     >
-      <div className={ props.className == "reverse" ? classes.paragraphWrapperReverse : classes.paragraphWrapper}>
+      <div
+        className={
+          props.className == "reverse"
+            ? classes.paragraphWrapperReverse
+            : classes.paragraphWrapper
+        }
+      >
         {props.title && <h1>{props.title}</h1>}
         {props.subtitle && <h2>{props.subtitle}</h2>}
         <p>{props.paragraph1}</p>
-        <p>{props.paragraph2}</p>
-        <p>{props.paragraph3}</p>
-        <p>{props.paragraph4}</p>
-        {props.buttonPath && (
+        {props.paragraph2 && <p>{props.paragraph2}</p>}
+        {props.paragraph3 && <p>{props.paragraph3}</p>}
+        {props.paragraph4 && <p>{props.paragraph4}</p>}
+        {props.buttonPosition && (
           <div className={classes.buttonContainer}>
             <Button href={props.buttonPath}>{props.buttonText}</Button>
           </div>
         )}
       </div>
-      <Image
-        className={classes.image}
-        src={`/${props.image}`}
-        alt="alt"
-        width={2048}
-        height={1400}
-      />
+
+      <div className={classes.imageButton}>
+        <Image
+          className={classes.image}
+          src={`/${props.image}`}
+          alt="alt"
+          width={2048}
+          height={1400}
+        />
+        {(props.buttonPath && !props.buttonPosition) && (
+          <div className={classes.buttonContainer}>
+            <Button href={props.buttonPath}>{props.buttonText}</Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
