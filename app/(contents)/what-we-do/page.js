26c6654927ebf4575dd675d2978520c853436909
KasiaRaven26 <rejven777@gmail.com>
2024-10-29 22:39:43 +0000

    dd

--- app/(contents)/what-we-do/page.js
+++ app/(contents)/what-we-do/page.js
@@ -9,67 +9,60 @@ export default function WhatWeDoPage() {
       <div className={classes.subHeader}>
         <h1>WHAT WE DO</h1>
       </div>
-      <section className={classes.section1}>
-        <WebItemComponent
-          title={whatWeDoData[0]["title"]}
-          paragraph1={whatWeDoData[0]["info"]["paragraph"]}
-          paragraph2={whatWeDoData[0]["info"]["paragraph2"]}
-          image={whatWeDoData[0]["image"]}
-        />
-        <hr />
-        <WebItemComponent
-          title={whatWeDoData[1]["title"]}
-          paragraph1={whatWeDoData[1]["info"]["paragraph"]}
-          image={whatWeDoData[1]["image"]}
-        />
-         <hr />
-        <WebItemComponent
-          title={whatWeDoData[2]["title"]}
-          paragraph1={whatWeDoData[2]["info"]["paragraph"]}
-          paragraph2={whatWeDoData[2]["info"]["paragraph2"]}
-          image={whatWeDoData[2]["image"]}
-        />
-         <hr />
-        <WebItemComponent
-          title={whatWeDoData[3]["title"]}
-          paragraph1={whatWeDoData[3]["info"]["paragraph"]}
-          paragraph2={whatWeDoData[3]["info"]["paragraph2"]}
-          paragraph3={whatWeDoData[3]["info"]["paragraph3"]}
-          image={whatWeDoData[3]["image"]}
-          buttonText={whatWeDoData[3]["button"]["text"]}
-          buttonPath={whatWeDoData[3]["button"]["href"]}
-        />
-         <hr />
-        <WebItemComponent
-          title={whatWeDoData[4]["title"]}
-          paragraph1={whatWeDoData[4]["info"]["paragraph"]}
-          paragraph2={whatWeDoData[4]["info"]["paragraph2"]}
-          image={whatWeDoData[4]["image"]}
-          buttonText={whatWeDoData[4]["button"]["text"]}
-          buttonPath={whatWeDoData[4]["button"]["href"]}
-        />
-         <hr />
-        <WebItemComponent
-          buttonPosition="left"
-          title={whatWeDoData[5]["title"]}
-          paragraph1={whatWeDoData[5]["info"]["paragraph"]}
-          image={whatWeDoData[4]["image"]}
-          buttonText={whatWeDoData[5]["button"]["text"]}
-          buttonPath={whatWeDoData[5]["button"]["href"]}
-        />
-         <hr />
-        <WebItemComponent
-          title={whatWeDoData[6]["title"]}
-          paragraph1={whatWeDoData[6]["info"]["paragraph"]}
-          image={whatWeDoData[6]["image"]}
-        />
-         <hr />
-        <WebItemComponent
-          title={whatWeDoData[7]["title"]}
-          paragraph1={whatWeDoData[7]["info"]["paragraph"]}
-          image={whatWeDoData[7]["image"]}
-        />
-      </section>
+      <WebItemComponent
+        title={whatWeDoData[0]["title"]}
+        paragraph1={whatWeDoData[0]["info"]["paragraph"]}
+        paragraph2={whatWeDoData[0]["info"]["paragraph2"]}
+        image={whatWeDoData[0]["image"]}
+      />
+      <WebItemComponent
+        title={whatWeDoData[1]["title"]}
+        paragraph1={whatWeDoData[1]["info"]["paragraph"]}
+        image={whatWeDoData[1]["image"]}
+      />
+      <WebItemComponent
+        title={whatWeDoData[2]["title"]}
+        paragraph1={whatWeDoData[2]["info"]["paragraph"]}
+        paragraph2={whatWeDoData[2]["info"]["paragraph2"]}
+        image={whatWeDoData[2]["image"]}
+      />
+      <WebItemComponent
+        buttonPosition="left"
+        title={whatWeDoData[3]["title"]}
+        paragraph1={whatWeDoData[3]["info"]["paragraph"]}
+        paragraph2={whatWeDoData[3]["info"]["paragraph2"]}
+        // paragraph3={whatWeDoData[3]["info"]["paragraph3"]}
+        image={whatWeDoData[3]["image"]}
+        buttonText={whatWeDoData[3]["button"]["text"]}
+        buttonPath={whatWeDoData[3]["button"]["href"]}
+      />
+      <WebItemComponent
+        buttonPosition="left"
+        title={whatWeDoData[4]["title"]}
+        paragraph1={whatWeDoData[4]["info"]["paragraph"]}
+        paragraph2={whatWeDoData[4]["info"]["paragraph2"]}
+        image={whatWeDoData[4]["image"]}
+        buttonText={whatWeDoData[4]["button"]["text"]}
+        buttonPath={whatWeDoData[4]["button"]["href"]}
+      />
+      <WebItemComponent
+        buttonPosition="left"
+        title={whatWeDoData[5]["title"]}
+        paragraph1={whatWeDoData[5]["info"]["paragraph"]}
+        image={whatWeDoData[4]["image"]}
+        buttonText={whatWeDoData[5]["button"]["text"]}
+        buttonPath={whatWeDoData[5]["button"]["href"]}
+      />
+      <WebItemComponent
+        title={whatWeDoData[6]["title"]}
+        paragraph1={whatWeDoData[6]["info"]["paragraph"]}
+        image={whatWeDoData[6]["image"]}
+      />
+      <WebItemComponent
+        title={whatWeDoData[7]["title"]}
+        paragraph1={whatWeDoData[7]["info"]["paragraph"]}
+        image={whatWeDoData[7]["image"]}
+      />
     </>
   );
 }
--- data/who-we-are-data.js
+++ data/who-we-are-data.js
@@ -6,14 +6,14 @@ export const whoWeAreData = [
       paragraph: `INDIGO was established in 2021 by Emmanuel
         Petrakis. Stands as a bespoke real estate consulting firm
         capitalizing on strategic investment opportunities. Boasting 25
-        years of experience and local knowledge.. `,
+        years of experience and local knowledge. `,
       paragraph2: `Indigo serves as a trusted service company to investors,
         providing expertise consulting in residential development. The
         company’s multi-disciplinary team, comprised of investment,
         finance and realty professionals, collaborates with reputable
         firms such as developers, architects, lawyers, notaries and
         more.`,
-        paragraph3: `The clientele includes private investors and investment funds,
+      paragraph3: `The clientele includes private investors and investment funds,
           both local and foreign, seeking tailored solutions for various
           real estate ventures. Indigo emphasizes in transparency,
           low-risk strategies, and a commitment to guide clients through
@@ -31,7 +31,7 @@ export const whoWeAreData = [
     info: {
       paragraph: `With more than 25 years of extensive local
           expertise spanning diverse business sectors I have cultivated
-          a deep understanding of the investment landscape. `,
+          a deep understanding of the investment landscape.`,
       paragraph2: `Over the last 18 years my focus has extended to
           encompass real estate.`,
       paragraph3: `Having served as CEO within this quarter-century
