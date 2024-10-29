3ab5be8a3d7e56b94aed933872e81f3becfcf933
KasiaRaven26 <rejven777@gmail.com>
2024-10-29 22:50:42 +0000

    test

--- server/actions/login.js
+++ server/actions/login.js
@@ -16,49 +16,49 @@ export async function Login(state, formData) {
   // console.log(passwords);
   // const sessions = await db.session.findMany();
   // console.log(sessions);
-  const username = formData.get("username");
-  const password = formData.get("password");
+  // const username = formData.get("username");
+  // const password = formData.get("password");
 
-  let errors = [];
+  // let errors = [];
 
-  const existingUser = await db.user.findFirst({
-    where: { username: username },
-  });
+  // const existingUser = await db.user.findFirst({
+  //   where: { username: username },
+  // });
 
-  if (!existingUser) {
-    errors.push("Invalid username or password");
-    return { errors };
-  }
-  console.log(existingUser);
+  // if (!existingUser) {
+  //   errors.push("Invalid username or password");
+  //   return { errors };
+  // }
+  // console.log(existingUser);
 
-  const userpasswords = await db.password.findFirst({
-    where: { userId: existingUser.id },
-  });
+  // const userpasswords = await db.password.findFirst({
+  //   where: { userId: existingUser.id },
+  // });
 
-  console.log(userpasswords);
+  // console.log(userpasswords);
 
-  const validPassword = await new LegacyScrypt().verify(userpasswords.hashedPassword, password);
-  console.log(validPassword)
+  // const validPassword = await new LegacyScrypt().verify(userpasswords.hashedPassword, password);
+  // console.log(validPassword)
 
-  if (!validPassword) {
-    errors.push("Invalid username or password");
-    return { errors };
-  }
+  // if (!validPassword) {
+  //   errors.push("Invalid username or password");
+  //   return { errors };
+  // }
 
-  const userId = existingUser.id;
-  const session = await lucia.createSession(
-     userId
-  );
-  const sessions = await db.session.findMany();
-  console.log(sessions)
+  // const userId = existingUser.id;
+  // const session = await lucia.createSession(
+  //    userId
+  // );
+  // const sessions = await db.session.findMany();
+  // console.log(sessions)
 
-  const sessionCookie = lucia.createSessionCookie(session.id);
-  console.log(sessionCookie.name, "Cookie name");
-  cookies().set(
-    sessionCookie.name,
-    sessionCookie.value,
-    sessionCookie.attributes
-  );
+  // const sessionCookie = lucia.createSessionCookie(session.id);
+  // console.log(sessionCookie.name, "Cookie name");
+  // cookies().set(
+  //   sessionCookie.name,
+  //   sessionCookie.value,
+  //   sessionCookie.attributes
+  // );
 
   // if (state.redirection) {
   //   redirect(`/${state.redirection}`);
@@ -66,5 +66,5 @@ export async function Login(state, formData) {
   // if (existingUser.admin_access == 2) {
   //   return redirect("/admin");
   // }
-  // return redirect("/");
+  return redirect("/");
 }
