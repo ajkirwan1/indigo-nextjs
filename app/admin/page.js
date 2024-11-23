60b8b06097b82bbab3c2fde261588e8276b280a4
KasiaRaven26 <rejven777@gmail.com>
2024-11-23 13:39:10 +0000

    commit

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,44 +1,112 @@
 /** @format */
 
-import { validateRequest } from "@/auth/lucia";
-import Table from "@/components/layouts/table/table";
-import { redirect } from "next/navigation";
-import PrepareAdminClientData from "@/utils/admin-table-data";
-import { FindAllUsers } from "@/server/actions/find-all-users";
+"use client";
+
 import classes from "./page.module.css";
-import { Suspense } from "react";
-import TableFallback from "@/components/fallbacks/table-fallback";
+import { useState } from "react";
+import GetPropertyImage from "@/components/forms/admin/properties/get-property-image";
+import GetPropertyPdf from "@/components/forms/admin/properties/get-property-pdf";
+import AddPropertyInfo from "@/components/forms/admin/properties/add-property-info";
+import { SubmitPropertyAction } from "@/server/actions/db/admin/properties/submit/submit-property-action";
 
-async function TableData() {
-  await new Promise((resolve) => setTimeout(resolve, 4000));
-  const resp = await FindAllUsers();
+import SubmitButton from "@/components/ui/buttons/submit-button";
+import Link from "next/link";
 
-  const { headerData, bodyData } = PrepareAdminClientData(resp);
-  const theadData = [...headerData];
-  const tbodyData = [...bodyData];
+export default function AddProperties() {
+  const [data, setData] = useState({
+    title: "",
+    location: "",
+    price: "",
+    description: "",
+    pdfUrl: "",
+    imageUrl: "",
+  });
+
+  const handleChange = (event) => {
+    console.log(data);
+    const { name, value } = event.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handlePdfUrl = (Url) => {
+    setData({
+      ...data,
+      pdfUrl: Url,
+    });
+  };
+
+  const handleImageUrl = (Url) => {
+    setData({
+      ...data,
+      imageUrl: Url,
+    });
+  };
+
+  const handleSubmitProperty = () => {
+    SubmitPropertyAction(data);
+  };
 
   return (
-    <Table theadData={theadData} tbodyData={tbodyData} customClass="admin" />
-  );
-}
-
-export default async function AdminPage() {
-  const { user } = await validateRequest();
-  console.log("ADMIN USER", user);
-
-  if (!user) {
-    redirect("/");
-  }
-  if (user?.adminaccess != 2) {
-    redirect("/");
-  }
-
-  return (
-    <div className={classes.tableContainer}>
-      <h1>LIST OF REGISTERED USERS</h1>
-      <Suspense fallback={<TableFallback />}>
-        <TableData />
-      </Suspense>
-    </div>
+    <>
+      <div className={classes.subHeader}>
+        <h1>Admin Dashboard</h1>
+      </div>
+      <div className={classes.itemWrapper}>
+        <div className={classes.infoContainer}>
+          <h2>Clients</h2>
+          <p>Links and information related to clients who have registered</p>
+          <div className={classes.linkWrapper}>
+            <Link href="/admin/user">All clients</Link>
+          </div>
+        </div>
+        {/* <GetPropertyImage handleImageUrl={handleImageUrl} /> */}
+      </div>
+      <div className={classes.itemWrapper}>
+        <div className={classes.infoContainer}>
+          <h2>Properties</h2>
+          <p>Links and information related to your properties</p>
+          <div className={classes.linkWrapper}>
+            <Link href="/admin/properties">All properties</Link>
+          </div>
+          {/* <div className={classes.linkWrapper}>
+            <p>
+              If you have not yet uploaded the desired pdf to the database, you
+              can upload it here:
+            </p>
+            <Link href="/">UPLOAD PDF</Link>
+          </div> */}
+        </div>
+        {/* <GetPropertyPdf handlePdfUrl={handlePdfUrl} /> */}
+      </div>
+      <div className={classes.itemWrapper}>
+        <div className={classes.infoContainer}>
+          <h2>Messages</h2>
+          <p>Links and information related to your messages</p>
+          <div className={classes.linkWrapper}></div>
+        </div>
+        {/* <AddPropertyInfo data={data} handleChange={handleChange} /> */}
+      </div>
+      <div className={classes.itemWrapper}>
+        <div className={classes.infoContainer}>
+          <h2>Step 3 - Provide property details</h2>
+          <p>Please fill-in all fields</p>
+          <div className={classes.linkWrapper}></div>
+        </div>
+        {/* <AddPropertyInfo data={data} handleChange={handleChange} /> */}
+      </div>
+      {/* <div className={classes.itemWrapper}>
+        <div className={classes.infoContainer}>
+          <h2>Step 4 - Submit</h2>
+          <p>Submit the property to the database</p>
+          <div className={classes.linkWrapper}></div>
+        </div>
+        <div className={classes.submitButtonContainer}>
+          <button onClick={handleSubmitProperty}>Add Property</button>
+        </div>
+      </div> */}
+    </>
   );
 }
