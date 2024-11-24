da8dadb8f177e91e29cfef69b72109643683c64c
ajkirwan1 <ajkirwan1@gmail.com>
2024-11-24 17:41:27 +0000

    commit

--- server/actions/s3/images/get-all-images-s3.js
+++ server/actions/s3/images/get-all-images-s3.js
@@ -23,7 +23,7 @@ export async function getAllObjectsSignedUrlsS3({ Bucket }) {
       (listObjectsOutput.Contents || []).map(async (object) => {
         if (object.Key) {
           const getObjectCommand = new GetObjectCommand({
-            Bucket: process.env.AWS_BUCKET_NAME_PDF,
+            Bucket: "next-js-image-bucket",
             Key: object.Key,
           });
           const url = await getSignedUrl(s3, getObjectCommand, {
