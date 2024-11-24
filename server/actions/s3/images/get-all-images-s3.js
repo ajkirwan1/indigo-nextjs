7ac688729e9083c777b219a842f52c0af671abbd
ajkirwan1 <ajkirwan1@gmail.com>
2024-11-24 17:07:35 +0000

    commit

--- server/actions/s3/images/get-all-images-s3.js
+++ server/actions/s3/images/get-all-images-s3.js
@@ -1,3 +1,4 @@
+'use server'
 import { GetObjectCommand } from "@aws-sdk/client-s3";
 import { ListObjectsV2Command } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
@@ -22,7 +23,7 @@ export async function getAllObjectsSignedUrlsS3({ Bucket }) {
       (listObjectsOutput.Contents || []).map(async (object) => {
         if (object.Key) {
           const getObjectCommand = new GetObjectCommand({
-            Bucket,
+            Bucket: Bucket,
             Key: object.Key,
           });
           const url = await getSignedUrl(s3, getObjectCommand, {
