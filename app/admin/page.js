0484b33789ac45ff29a04d69fbe74b2fcf375fb7
KasiaRaven26 <rejven777@gmail.com>
2024-11-25 11:47:54 +0000

    commitedonmonday

--- app/admin/page.js
+++ app/admin/page.js
@@ -9,11 +9,11 @@ export default function AddProperties() {
   return (
     <>
       <div className={classes.subHeader}>
-        <h1>Admin Dashboard</h1>
+        <h1>ADMIN DASHBOARD</h1>
       </div>
       <div className={classes.itemWrapper}>
         <div className={classes.infoContainer}>
-          <h2>Clients</h2>
+          <h2>CLIENTS</h2>
           <p>Links and information related to clients who have registered</p>
           <div className={classes.linkWrapper}>
             <Link href="/admin/user">All clients</Link>
@@ -22,7 +22,7 @@ export default function AddProperties() {
       </div>
       <div className={classes.itemWrapper}>
         <div className={classes.infoContainer}>
-          <h2>Properties</h2>
+          <h2>PROPERTIES</h2>
           <p>Links and information related to your properties</p>
           <div className={classes.linkWrapper}>
             <Link href="/admin/properties">All properties</Link>
@@ -31,21 +31,21 @@ export default function AddProperties() {
       </div>
       <div className={classes.itemWrapper}>
         <div className={classes.infoContainer}>
-          <h2>Messages</h2>
+          <h2>MESSAGES</h2>
           <p>Links and information related to your messages</p>
           <div className={classes.linkWrapper}></div>
         </div>
       </div>
       <div className={classes.itemWrapper}>
         <div className={classes.infoContainer}>
-          <h2>Blogs</h2>
+          <h2>BLOGS</h2>
           <p>Links and information related to your blogs</p>
           <div className={classes.linkWrapper}></div>
         </div>
       </div>
       <div className={classes.itemWrapper}>
         <div className={classes.infoContainer}>
-          <h2>Projects</h2>
+          <h2>PROJECTS</h2>
           <p>Links and information related to your Projects</p>
           <div className={classes.linkWrapper}></div>
         </div>
