d0381def85ceb438674c4144c154d7fe2875a9fc
KasiaRaven26 <rejven777@gmail.com>
2024-12-10 12:34:45 +0000

    decora

--- app/(contents)/what-we-do/page.js
+++ app/(contents)/what-we-do/page.js
@@ -5,103 +5,52 @@ import classes from "./page.module.css";
 import { whatWeDoData } from "@/data/what-we-do-data";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import Link from "next/link";
-import Overlay from "@/components/overlay";
-import circle from "/public/images/pages/home/circledwh.png";
-import WhatWeDoCarousel from "@/components/pages/what-we-do/what-we-do-carousel";
-import { propertyData } from "@/public/data/data";
-import { motion } from "framer-motion";
-
-const container = {
-  hidden: { opacity: 0, y: 10 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      staggerChildren: 0.5,
-      delayChildren: 0.5,
-      // delay: 0.5,
-      bounce: 0,
-    },
-  },
-};
-
-const item = {
-  hidden: { opacity: 0, y: 30 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      bounce: 0,
-    },
-  },
-};
-
-function Modal({ handleModal, modalIndex }) {
-  console.log(modalIndex);
-  return (
-    <div className="modal-backdrop" onClick={handleModal}>
-      <div className={classes.overlayWrapper}>
-        <Overlay>
-          <div className={classes.modalInfoWrapper}>
-            <Image src={circle} />
-            <h2>{whatWeDoData[modalIndex].title}</h2>
-            <p>{whatWeDoData[modalIndex].info.paragraph}</p>
-            <p>{whatWeDoData[modalIndex].info.paragraph2}</p>
-            <p>{whatWeDoData[modalIndex].info.paragraph3}</p>
-          </div>
-        </Overlay>
-      </div>
-    </div>
-  );
-}
 
 function ServiceItem({ data, handleModal, infoActive, modalIndex }) {
   return (
-    <div key={data.id}>
-      <div
-        className={
-          !(infoActive && data.id == modalIndex)
-            ? `${classes.itemContainer}`
-            : null
-        }
-      >
-        <div className={classes.imageContainer}>
-          <Image
-            className={classes.image}
-            src={data.image}
-            alt="alt"
-            width={1000}
-            height={1250}
-          />
-          <div className={classes.moreContainer}>
-            {infoActive && data.id == modalIndex ? null : <h2>{data.title}</h2>}
-          </div>
-          <div
-            className={
-              infoActive && data.id == modalIndex
-                ? `${classes.infoWrapper} ${classes.infoWrapperActive}`
-                : `${classes.infoWrapper} `
-            }
-          >
-            {!(infoActive && data.id == modalIndex) ? (
-              <div className={classes.popupContainerClosed}>
-                <p onClick={() => handleModal(data.id)}>More</p>
+    <div
+      key={data.id}
+      className={
+        !(infoActive && data.id == modalIndex)
+          ? `${classes.itemContainer}`
+          : null
+      }
+    >
+      <div className={classes.imageContainer}>
+        <Image
+          className={classes.image}
+          src={data.image}
+          alt="alt"
+          width={1000}
+          height={1250}
+        />
+        <div className={classes.moreContainer}>
+          {infoActive && data.id == modalIndex ? null : <h2>{data.title}</h2>}
+        </div>
+        <div
+          className={
+            infoActive && data.id == modalIndex
+              ? `${classes.infoWrapper} ${classes.infoWrapperActive}`
+              : `${classes.infoWrapper} `
+          }
+        >
+          {!(infoActive && data.id == modalIndex) ? (
+            <div className={classes.popupContainerClosed}>
+              <p onClick={() => handleModal(data.id)}>More</p>
+            </div>
+          ) : (
+            <div className={classes.popupContainerOpen}>
+              <div>
+                <h2>{whatWeDoData[modalIndex].title}</h2>
+                <p>{whatWeDoData[modalIndex].info.paragraph}</p>
+                <p>{whatWeDoData[modalIndex].info.paragraph2}</p>
+                <p>{whatWeDoData[modalIndex].info.paragraph3}</p>
               </div>
-            ) : (
-              <div className={classes.popupContainerOpen}>
-                <div>
-                  <h2>{whatWeDoData[modalIndex].title}</h2>
-                  <p>{whatWeDoData[modalIndex].info.paragraph}</p>
-                  <p>{whatWeDoData[modalIndex].info.paragraph2}</p>
-                  <p>{whatWeDoData[modalIndex].info.paragraph3}</p>
-                </div>
-                <div className={classes.lessContainer}>
-                  <p onClick={() => handleModal(data.id)}>Less</p>
-                </div>
+              <div className={classes.lessContainer}>
+                <p onClick={() => handleModal(data.id)}>Less</p>
               </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -110,7 +59,6 @@ function ServiceItem({ data, handleModal, infoActive, modalIndex }) {
 
 export default function ServicePage() {
   const [viewport, setViewport] = useState();
-  const [modalOpen, setModalOpen] = useState(false);
   const [modalIndex, setModalIndex] = useState(null);
   const [infoActive, setInfoActive] = useState(false);
 
@@ -139,35 +87,30 @@ export default function ServicePage() {
   const handleModal = (id) => {
     setInfoActive((val) => !val);
     setModalIndex(id);
-    // console.log(modalIndex);
   };
 
   return (
     <>
       <title>INDIGO CONSULTING WHAT WE DO</title>
-      {/* {modalOpen ? (
-        <Modal handleModal={handleModal} modalIndex={modalIndex} />
-      ) : null} */}
       <div className={classes.header}>
         <h1>SERVICES</h1>
       </div>
-      <section
-        className={classes.page}
-        layout
-        variants={container}
-        initial="hidden"
-        whileInView="show"
-      >
+      {/* <section>
+        <Image
+          src={"/images/pages/what-we-do/16:9/hands.jpg"}
+          alt="asdsa"
+          height={500}
+          width={1000}
+        />
+      </section> */}
+      <section className={classes.page}>
         {whatWeDoData.map((element) => (
-          <div variants={item} key={item}>
-            {/* {modalOpen ? <Modal handleModal={handleModal} /> : null} */}
-            <ServiceItem
-              data={element}
-              handleModal={handleModal}
-              modalIndex={modalIndex}
-              infoActive={infoActive}
-            />
-          </div>
+          <ServiceItem
+            data={element}
+            handleModal={handleModal}
+            modalIndex={modalIndex}
+            infoActive={infoActive}
+          />
         ))}
       </section>
     </>
