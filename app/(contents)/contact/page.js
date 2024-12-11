7f333b14380adf24f8a6d47ef6ac5861f0d2b73f
KasiaRaven26 <rejven777@gmail.com>
2024-12-11 11:26:50 +0000

    commit

--- app/(contents)/contact/page.js
+++ app/(contents)/contact/page.js
@@ -9,14 +9,14 @@ export default function ContactUsPage() {
   return (
     <>
       <title>INDIGO CONSULTING CONTACT US PAGE</title>
-        <div className={classes.pageWrapper}>
-          <p>
-            Feel free to fill out the contact form below and one of our team
-            members will get back to you as soon as possible. Alternatively, you
-            can find our contact details listed below if you prefer to reach out
-            to us directly.
-          </p>
-        <div className={classes.wrapper}>
+      <div className={classes.pageWrapper}>
+        <p>
+          Feel free to fill out the contact form below and one of our team
+          members will get back to you as soon as possible. Alternatively, you
+          can find our contact details listed below if you prefer to reach out
+          to us directly.
+        </p>
+        <div className={`${classes.wrapper} ${classes.wrapperViewport}`}>
           <div className={classes.form}>
             <div className={classes.formcontainer}>
               <ContactForm action={ContactUs}></ContactForm>
@@ -25,14 +25,20 @@ export default function ContactUsPage() {
           <span></span>
           <div className={classes.letsTalk}>
             <h2>LET&apos;S TALK</h2>
-            <Image alt="An icon showing a phone" src={phoneIcon} className={classes.phoneIcon} />
+            <Image
+              alt="An icon showing a phone"
+              src={phoneIcon}
+              className={classes.phoneIcon}
+            />
             <div className={classes.phoneAndEmail}>
-              <h1>PHONE: +30 697 333 5888</h1>
-              <h1>E-MAIL: EP@INDIGO-CONSULTING.GR</h1>
+              <h3>PHONE: +30 697 333 5888</h3>
+              <h3>E-MAIL: EP@INDIGO-CONSULTING.GR</h3>
             </div>
           </div>
         </div>
-        </div>
+
+        <div className={`${classes.mobileWrapper}`}>HELLOW</div>
+      </div>
     </>
   );
 }
