21db0dbf0afdbb598a283eea4b4f975aab02c00a
Adam Kirwan <ajkirwan1@gmail.com>
2024-12-22 09:45:38 +0000

    commit

--- server/actions/contact.js
+++ server/actions/contact.js
@@ -1,16 +1,11 @@
 /** @format */
 "use server";
 
-import { validateRequest } from "@/auth/lucia";
-import { cookies } from "next/headers";
-import { lucia } from "@/auth/lucia";
-import { redirect } from "next/navigation";
-import db from "@/modules/db";
 import { sendMail } from "@/lib/send-mail";
 
 export async function ContactUs(_, formData) {
   const message = formData.get("message");
-  const email = formData.get("password");
+  const email = formData.get("email");
   const firstName = formData.get("firstName");
   const lastName = formData.get("lastName");
   const contactNumber = formData.get("contactNumber");
@@ -22,14 +17,15 @@ export async function ContactUs(_, formData) {
     message.length < 6 ||
     message.length > 255
   ) {
-    // errors.push("Invalid message");
     errors.push({ errorType: "message", message: "Invalid message" });
   }
 
   if (
-    typeof email !== "email" ||
+    typeof email !== "string" ||
     email.length < 6 ||
-    email.length > 25 
+    email.length > 25 ||
+    !email.includes("@") ||
+    !email.includes(".")
   ) {
     errors.push({ errorType: "email", message: "Invalid email" });
   }
@@ -55,15 +51,17 @@ export async function ContactUs(_, formData) {
     contactNumber.length < 6 ||
     contactNumber.length > 30
   ) {
-    errors.push({ errorType: "contactNumber", message: "Invalid contact number" });
+    errors.push({
+      errorType: "contactNumber",
+      message: "Invalid contact number",
+    });
   }
-
-  console.log(errors);
-  console.log(errors.find((item) => item.errorType == "email"));
 
   if (errors.length > 0) {
     return { errors };
   }
+  const submitted = true;
+  return { submitted };
 
   // const response = await sendMail({
   //   email: "ajkirwan1gmail.com",
@@ -72,24 +70,5 @@ export async function ContactUs(_, formData) {
   //   text: message
   // })
 
-  // const sessions = await db.session.findMany();
-
-  //   const sessions = await db.session.findMany();
-  //   console.log(sessions);
-  //   const { session } = await validateRequest();
-  //   if (!session) {
-  //     return {
-  //       error: "Unauthorized",
-  //     };
-  //   }
-
-  //   await lucia.invalidateSession(session.id);
-
-  //   const sessionCookie = lucia.createBlankSessionCookie();
-  //   cookies().set(
-  //     sessionCookie.name,
-  //     sessionCookie.value,
-  //     sessionCookie.attributes
-  //   );
   // return redirect("/");
 }
