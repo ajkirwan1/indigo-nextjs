9f124073721c1e65ba8ac3cd9b6223ebfd98f74b
Adam Kirwan <ajkirwan1@gmail.com>
2024-12-30 19:02:30 +0000

    commit

--- app/(contents)/contact/page.js
+++ app/(contents)/contact/page.js
@@ -9,45 +9,38 @@ import phoneIcon from "/public/images/icons/phoneblue.png";
 import contactIcon from "/public/images/icons/envelope.png";
 
 export default function ContactUsPage() {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const handleModalToggle = () => {
-    setModalOpen((val) => !val);
-  };
-
   return (
     <>
-      <div
-        className={
-          !modalOpen
-            ? `${classes.ModalBackdropWrapper}`
-            : `${classes.ModalBackdropWrapperOpen}`
-        }
-        onClick={handleModalToggle}
-      >
-        <div className={classes.form}>
-          <div className={classes.formcontainer}>
-            <ContactForm action={ContactUs}></ContactForm>
+      <title>INDIGO CONSULTING CONTACT US PAGE</title>
+      <div className={classes.pageWrapper}>
+        <p>
+          Feel free to fill out the contact form below and one of our team
+          members will get back to you as soon as possible. Alternatively, you
+          can find our contact details listed below if you prefer to reach out
+          to us directly.
+        </p>
+        <div className={`${classes.wrapper} ${classes.wrapperViewport}`}>
+          <div className={classes.form}>
+            <div className={classes.formcontainer}>
+              <ContactForm action={ContactUs}></ContactForm>
+            </div>
+          </div>
+          <span></span>
+          <div className={classes.letsTalk}>
+            <h2>LET&apos;S TALK</h2>
+            <Image
+              alt="An icon showing a phone"
+              src={phoneIcon}
+              className={classes.phoneIcon}
+            />
+            <div className={classes.phoneAndEmail}>
+              <h3>PHONE: +30 697 333 5888</h3>
+              <h3>E-MAIL: EP@INDIGO-CONSULTING.GR</h3>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div>
-        <title>INDIGO CONSULTING CONTACT US PAGE</title>
-        <div className={classes.pageWrapper}>
-          <p>
-            Feel free to fill out the contact form below and one of our team
-            members will get back to you as soon as possible. Alternatively, you
-            can find our contact details listed below if you prefer to reach out
-            to us directly.
-          </p>
-          <div className={`${classes.wrapper} ${classes.wrapperViewport}`}>
-            <div className={classes.form}>
-              <div className={classes.formcontainer}>
-                <ContactForm action={ContactUs}></ContactForm>
-              </div>
-            </div>
-            <span></span>
+        <div>
+          <div className={classes.formMobile}>
             <div className={classes.letsTalk}>
               <h2>LET&apos;S TALK</h2>
               <Image
@@ -60,29 +53,8 @@ export default function ContactUsPage() {
                 <h3>E-MAIL: EP@INDIGO-CONSULTING.GR</h3>
               </div>
             </div>
-          </div>
-          <div className={`${classes.mobileWrapper}`}>
-            <div
-              className={classes.mobileMessageContainer}
-              onClick={handleModalToggle}
-            >
-              <h2>Message</h2>
-              <Image
-                alt="An icon showing a phone"
-                src={contactIcon}
-                className={classes.phoneIcon}
-              />
-            </div>
-            <div
-              className={classes.mobileMessageContainer}
-              onClick={handleModalToggle}
-            >
-              <h2>Let&apos;s talk</h2>
-              <Image
-                alt="An icon showing a phone"
-                src={phoneIcon}
-                className={classes.phoneIcon}
-              />
+            <div className={classes.formcontainer}>
+              <ContactForm action={ContactUs}></ContactForm>
             </div>
           </div>
         </div>
