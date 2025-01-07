bc9bfadd38b789b25f2779f68536c476a10a0ffc
Adam Kirwan <ajkirwan1@gmail.com>
2025-01-07 19:43:13 +0000

    commit

--- components/pages/account/account-personal-details.js
+++ components/pages/account/account-personal-details.js
@@ -2,27 +2,36 @@
 
 import classes from "./account-personal-details.module.css";
 import ClientPersonalDetailsForm from "@/components/forms/account/client-personal-details-form";
+import RequestFallbackReset from "@/components/fallbacks/admin/request-fallback-reset";
 import { getUser } from "@/server/actions/db/client";
 
 export default async function ClientAccountPersonalDetails({ id }) {
   await new Promise((resolve) => setTimeout(resolve, 3000));
-  const user = await getUser(id);
+  const result = await getUser(id);
 
-  const { username, firstname, lastname, email, companyname, phonenumber } =
-    user;
+  // if (!result.dbFetchError) {
+  //   const { username, firstname, lastname, email, companyname, phonenumber } =
+  //     user;
+  // }
 
   return (
-    <div className={classes.outerWrapper}>
-      <h2>Personal details</h2>
-      <ClientPersonalDetailsForm
-        username={username}
-        firstname={firstname}
-        lastname={lastname}
-        email={email}
-        companyname={companyname}
-        phonenumber={phonenumber}
-        id={id}
-      />
-    </div>
+    <>
+      {result?.dbFetchError ? (
+        <RequestFallbackReset />
+      ) : (
+        <div className={classes.outerWrapper}>
+          <h2>Personal details</h2>
+          <ClientPersonalDetailsForm
+            username={user.username}
+            firstname={user.firstname}
+            lastname={user.lastname}
+            email={user.email}
+            companyname={user.companyname}
+            phonenumber={user.phonenumber}
+            id={id}
+          />
+        </div>
+      )}
+    </>
   );
 }
--- components/pages/account/account-registration-details.js
+++ components/pages/account/account-registration-details.js
@@ -2,17 +2,28 @@
 
 import { getUser } from "@/server/actions/db/client";
 import ClientRegistrationDetailsForm from "@/components/forms/account/client-registration-details-form";
+import RequestFallbackReset from "@/components/fallbacks/admin/request-fallback-reset";
 import { UpdateUserAccountRegisrationInfo } from "@/server/actions/db/account-registration-update";
 import classes from "./account-registration-details.module.css";
 
 export default async function ClientRegistrationDetails({ id }) {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  const user = await getUser(id);
+  const result = await getUser(id);
 
   return (
-    <div className={classes.outerWrapper}>
-      <h2>Registration information</h2>
-      <ClientRegistrationDetailsForm id={id} user={user} action={UpdateUserAccountRegisrationInfo}/>
-    </div>
+    <>
+      {result?.dbFetchError ? (
+        <RequestFallbackReset />
+      ) : (
+        <div className={classes.outerWrapper}>
+          <h2>Registration information</h2>
+          <ClientRegistrationDetailsForm
+            id={id}
+            user={user}
+            action={UpdateUserAccountRegisrationInfo}
+          />
+        </div>
+      )}
+    </>
   );
 }
