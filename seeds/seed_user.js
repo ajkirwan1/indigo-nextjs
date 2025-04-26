5820590e473fa6adeb59dcce100922367b0f8b6f
Adam Kirwan <ajkirwan1@gmail.com>
2025-04-26 20:43:28 +0100

    commit

--- seeds/seed_user.js
+++ seeds/seed_user.js
@@ -32,7 +32,7 @@ export async function seed(knex) {
   await knex("usersonproperties").del();
   await knex("usersonproperties2").del();
   await knex("userRegistration").del();
-  await knex("usersNew").del();
+  await knex("userNew").del();
   await knex("Pdf").del();
   await knex("userPdf").del();
   await knex("users").insert([
