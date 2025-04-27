c6a0d03fc9624f2a6ba49b3ab667d5b8aa8c3e89
Adam Kirwan <ajkirwan1@gmail.com>
2025-04-27 15:03:00 +0100

    commit

--- middleware.js
+++ middleware.js
@@ -1,39 +1,43 @@
+/** @format */
+
 // export { auth as middleware } from "@/auth"
 import { NextResponse } from "next/server";
 import NextAuth from "next-auth";
-import {authConfig} from "./auth.config"
-import { getToken } from "next-auth/jwt"
+import { authConfig } from "./auth.config";
+import { getToken } from "next-auth/jwt";
 
 const protectedRoutes = ["/admin"];
-const adminRedirectRoutes = ["/", "/contact"]
+const adminRedirectRoutes = ["/", "/contact"];
 
 const { auth } = NextAuth(authConfig);
 
-export default auth(async function middleware(req) { 
+export default auth(async function middleware(req) {
+  // const session = await auth();
 
-    // const session = await auth();
-    
-    const token = await getToken({ req, secret:process.env.AUTH_SECRET })
-    console.log(process.env.AUTH_SECRET, "process.env.AUTH_SECRET")
-    console.log(token, "token")
-    const { pathname } = req.nextUrl;
-    
-    const shouldRedirectToAdmin = token?.role === "admin" && 
-    adminRedirectRoutes.some((route) => pathname === route || pathname.startsWith(route + "/"));
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  console.log("NEXTAUTH_SECRET:", process.env.NEXTAUTH_SECRET);
+  console.log("AUTH_SECRET:", process.env.AUTH_SECRET);
+  console.log(token, "token");
+  const { pathname } = req.nextUrl;
 
-    if (shouldRedirectToAdmin) {
-      return NextResponse.redirect(new URL("/admin", req.nextUrl));
-    }
-
-    // const token= await getToken({ req, secret:process.env.AUTH_SECRET })
-  
-  
-    const isProtected = protectedRoutes.some((route) =>
-      pathname.startsWith(route)
+  const shouldRedirectToAdmin =
+    token?.role === "admin" &&
+    adminRedirectRoutes.some(
+      (route) => pathname === route || pathname.startsWith(route + "/")
     );
 
-    if (isProtected && token?.role !== "admin") {
-      return NextResponse.redirect(new URL("/", req.nextUrl));
-    }
-    return NextResponse.next();
+  if (shouldRedirectToAdmin) {
+    return NextResponse.redirect(new URL("/admin", req.nextUrl));
+  }
+
+  // const token= await getToken({ req, secret:process.env.AUTH_SECRET })
+
+  const isProtected = protectedRoutes.some((route) =>
+    pathname.startsWith(route)
+  );
+
+  if (isProtected && token?.role !== "admin") {
+    return NextResponse.redirect(new URL("/", req.nextUrl));
+  }
+  return NextResponse.next();
 });
