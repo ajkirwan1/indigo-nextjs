becd2548177e214afc44007cfa3ce9ddec4ce587
Adam Kirwan <ajkirwan1@gmail.com>
2025-04-27 17:45:37 +0100

    commit

--- auth.config.js
+++ auth.config.js
@@ -1,9 +1,22 @@
 export const authConfig = {
-    pages: {
-      signIn: '/login',
-      signOut: '/logout'
+  pages: {
+    signIn: '/login',
+    signOut: '/logout'
+  },
+  session: { strategy: 'jwt' },
+  secret: process.env.NEXTAUTH_SECRET, // Use NEXTAUTH_SECRET here
+  providers: [],
+  cookies: {
+    sessionToken: {
+      name: process.env.NODE_ENV === 'production'
+        ? '__Secure-next-auth.session-token'
+        : 'next-auth.session-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: process.env.NODE_ENV === 'production',
+      },
     },
-    session: {strategy : 'jwt'},
-    providers: [],
-    secret: process.env.NEXTAUTH_SECRET,
-  }
+  },
+};
