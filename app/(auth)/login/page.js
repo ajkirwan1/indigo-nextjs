66dd06852fddc951f39c5b91b7dfaa0df7c430c7
ajkirwan1 <ajkirwan1@gmail.com>
2025-06-22 20:52:42 +0100

    commit

--- app/(auth)/login/page.js
+++ app/(auth)/login/page.js
@@ -3,6 +3,7 @@ import LoginForm from "@/components/forms/login-form";
 import HeroComponent from "@/components/hero/hero-component";
 import heroImage from "/public/images/croppednight.jpg";
 import classes from "./page.module.css";
+import { Suspense } from 'react';
 
 export default async function LoginPage() {
 
@@ -11,7 +12,9 @@ export default async function LoginPage() {
       <div className={classes.pageWrapper}>
         <HeroComponent heroImage={heroImage} altText="Alt text" header footer>
           <div className={classes.formcontainer}>
-            <LoginForm></LoginForm>
+          <Suspense fallback={<div>Loading login...</div>}>
+          <LoginForm />
+        </Suspense>
           </div>
         </HeroComponent>
       </div>
